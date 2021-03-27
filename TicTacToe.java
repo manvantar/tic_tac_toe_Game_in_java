1de9f7907e7b784e84953a0fd76709aa2ac982bd
manvantar <manukvshetty@gmail.com>
2021-03-27 09:36:57 +0530

    Taking an input from user

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class TicTacToe {
 
 	public static void main(String[] args) {
@@ -7,9 +7,25 @@ public class TicTacToe {
 		char[] tictac= {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
 		int s=tictac.length;
 		System.out.println(s);
-		
+		char a=chooseChar();
 		
 	}
+	static char chooseChar(){
+		Scanner s=new Scanner(System.in);
+		System.out.println("choose x or o");
+		char input=s.next().charAt(0);
+		
+		while(input !='x' && input != 'o') {	
+		System.out.println("please choose x or o");	
+		input=s.next().charAt(0);	
+		}
+		return input;
+		
+	} 
+	
+	
+	
+	
 	
 	
 	
