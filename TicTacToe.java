b562621a18c3d9b46156b7cd6a3e13c8294fa682
manvantar <manukvshetty@gmail.com>
2021-03-27 14:38:56 +0530

    tic tac game between two players

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,91 +1,104 @@
 package com.bridgelabz.tictactoe_game;
-import java.util.Scanner;
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToe {
 
 	public static void main(String[] args) {
+		int index=0,findwinner=0, won=0;
 		char[] tictac= {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
-		int s=tictac.length,index=0;
-		System.out.println(s);
-		int winner=0;
+		//System.out.println(s);
+		Random r=new Random();
+		int toss=r.nextInt(10)%2+1;
+		if(toss==1) {
+			//System.out.println("Player x won the toss");
+			//char a=chooseChar(1);
+		}
+		else {
+			//System.out.println("Player o won the toss");
+			//char a=chooseChar(2);
+		}
+
 		showBroad(tictac);
-		while(winner!=0) {
-
-			winner=checkBoard(tictac);
-			if(winner==0) {
-
-
-				char a=chooseChar();
-
-
-
+		int i =1;
+		while( won !=1) {
+			int winner0[]=checkBoard(tictac);
+			won=winner0[0];
+			if(won==0) {
+				char a;
+				a=chooseChar(i);
+				System.out.println(a+ "  player choose the place or index");
+				Scanner s1=new Scanner(System.in);
+				index=s1.nextInt();
 				char changed[]=moveIntoBoard(tictac,a,index);
+				int winner2[]=checkBoard(tictac);
+				won=winner2[0];
 				if(changed[1]=='N') {
+					while(changed[1]=='N') {
 
-					char changed1[]=moveIntoBoard(tictac,a,index);
-
+						System.out.println(a+ "  place is full choose the place or index");
+						index=s1.nextInt();
+						showBroad(tictac);
+						char changed1[]=moveIntoBoard(tictac,a,index);
+						changed[1]=changed1[1];
+						if(changed1[1]=='Y') {
+							tictac[index]=changed1[0];
+							showBroad(tictac);
+						}
+					}
 				}
 				else {
 					tictac[index]=changed[0];
-
+					showBroad(tictac);
 				}
 			}
+			i++;
+		}
+		int winner1[]=checkBoard(tictac);
+		won=winner1[0];
+		findwinner=winner1[1];
+		if(winner1[1]==1) {
 
+			System.out.println("x won");
+		}
+		else if(winner1[1]==2) {
+			System.out.println("o won");
 
 		}
 	}
 
-	private static int checkBoard(char[] tic) {
-		int check=0;
-		if((tic[1]=='x'&& tic[2]=='x'&& tic[3]=='x') ||(tic[1]=='o'&& tic[2]=='o'&& tic[3]=='o')) {
+	private static int[] checkBoard(char[] tic) {
+		int check[]={0,0};		
 
-			check=1;
+		if((tic[1]=='x'&& tic[2]=='x'&& tic[3]=='x') ||(tic[4]=='x'&& tic[5]=='x'&& tic[6]=='x')||
+				(tic[7]=='x'&& tic[8]=='x'&& tic[9]=='x')|| (tic[1]=='x'&& tic[4]=='x'&& tic[7]=='x')||
+				(tic[2]=='x'&& tic[5]=='x'&& tic[8]=='x')||(tic[3]=='x'&& tic[6]=='x'&& tic[9]=='x')||
+				(tic[1]=='x'&& tic[5]=='x'&& tic[9]=='x')||(tic[7]=='x'&& tic[5]=='x'&& tic[3]=='x')) {
+			check[1]=1;
+			check[0]=1;
 		}
-		else if((tic[4]=='x'&& tic[5]=='x'&& tic[6]=='x') ||(tic[4]=='o'&& tic[5]=='o'&& tic[6]=='o')) {
-
-			check=1;
+		else if((tic[1]=='o'&& tic[2]=='o'&& tic[3]=='o') ||(tic[4]=='o'&& tic[5]=='o'&& tic[6]=='o')||
+				(tic[7]=='o'&& tic[8]=='o'&& tic[9]=='o')||(tic[7]=='1'&& tic[4]=='o'&& tic[1]=='o')||
+				(tic[2]=='o'&& tic[5]=='o'&& tic[8]=='o')||(tic[3]=='o'&& tic[6]=='o'&& tic[9]=='o')||
+				(tic[1]=='o'&& tic[5]=='o'&& tic[9]=='o')||(tic[7]=='o'&& tic[5]=='o'&& tic[3]=='o')) {
+			check[1]=2;
+			check[0]=1;
 		}
-		else if((tic[7]=='x'&& tic[8]=='x'&& tic[9]=='x') ||(tic[7]=='o'&& tic[8]=='o'&& tic[9]=='o')) {
+		//System.out.println("check is "+ check[0]);
 
-			check=1;
-		}
-
-		else if((tic[1]=='x'&& tic[4]=='x'&& tic[7]=='x') ||(tic[7]=='1'&& tic[4]=='o'&& tic[7]=='o')) {
-
-			check=1;
-		}
-		else if((tic[2]=='x'&& tic[5]=='x'&& tic[8]=='x') ||(tic[2]=='o'&& tic[5]=='o'&& tic[8]=='o')) {
-
-			check=1;
-		}
-		else if((tic[3]=='x'&& tic[6]=='x'&& tic[9]=='x') ||(tic[3]=='o'&& tic[6]=='o'&& tic[9]=='o')) {
-
-			check=1;
-		}
-		else if((tic[1]=='x'&& tic[5]=='x'&& tic[9]=='x') ||(tic[1]=='o'&& tic[5]=='o'&& tic[9]=='o')) {
-
-			check=1;
-		}
-		else if((tic[7]=='x'&& tic[5]=='x'&& tic[3]=='x') ||(tic[7]=='o'&& tic[5]=='o'&& tic[3]=='o')) {
-
-			check=1;
-		}
-
-
-		return check;
+		return check ;
 	}
 
-	static char chooseChar(){
-		Scanner s=new Scanner(System.in);
-		System.out.println("choose x or o");
-		char input=s.next().charAt(0);
 
-		while(input !='x' && input != 'o') {	
-			System.out.println("please choose x or o");	
-			input=s.next().charAt(0);	
+	static char chooseChar(int a){
+		char usr;
+		int user=a%2;
+		if(user==1) {
+			usr='x';
 		}
-		return input;
+		else {
+			usr='o';
+		}
+		return usr;
 	} 
 	static void showBroad(char in[]){
 		int s=in.length;
@@ -101,11 +114,16 @@ public class TicTacToe {
 				System.out.println();
 			}
 		}
+
+		System.out.println();
+
 	}
 
 	static char[] moveIntoBoard(char in[],char value,int index) {
-		char ret[]= {' ',' '};
-		if(in[index]==' ') {
+		char ret[]= {' ','N'};
+		int s=in.length;
+
+		if(index<=s && in[index]==' ') {
 			ret[0]=value;
 			ret[1]='Y';
 			return ret;
